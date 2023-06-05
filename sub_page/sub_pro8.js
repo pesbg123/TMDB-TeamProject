b0d55c4b55257692aa75583392ac8256f5dab059
jrmun <jrmun123@gmail.com>
2023-06-05 14:06:58 +0900

    일단 서브페이지에서 검색기능 구현했습니다

--- sub_page/sub_pro8.js
+++ sub_page/sub_pro8.js
@@ -1,30 +1,30 @@
 // URL에서 id 추출
 const urlParams = new URLSearchParams(window.location.search);
-const movieId = urlParams.get('id');
+const movieId = urlParams.get("id");
 
 const options = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
+    accept: "application/json",
     Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOWJiOTJmNjQ4ZDQxOTExNTVkMTdjOGU0M2YyNWU2OCIsInN1YiI6IjY0NzIxZmM1ZGQ3MzFiMDBjMGJhYWU5MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Y3E-hG7rguBwXeMYwJDAuXRxZp8nBSidYbJb6AFhSf0',
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOWJiOTJmNjQ4ZDQxOTExNTVkMTdjOGU0M2YyNWU2OCIsInN1YiI6IjY0NzIxZmM1ZGQ3MzFiMDBjMGJhYWU5MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Y3E-hG7rguBwXeMYwJDAuXRxZp8nBSidYbJb6AFhSf0",
   },
 };
 
-const apiKey = 'e9bb92f648d4191155d17c8e43f25e68&language=ko';
+const apiKey = "e9bb92f648d4191155d17c8e43f25e68&language=ko";
 
-const movieDetailsContainer = document.getElementById('movie-details');
-
+const movieDetailsContainer = document.getElementById("movie-details");
+const searchBox = document.getElementById("search-box");
+const clickButton = document.getElementById("click-btn");
 const movieUrl = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=ko`;
 
 fetch(movieUrl, options)
   .then((response) => response.json())
   .then((data) => {
-    const movieTitle = data['title'];
-    const movieDesc = data['overview'];
-    const movieRate = Math.round(data['vote_average'] * 10) / 10;
-
-    const movieImg = data['poster_path'];
+    const movieTitle = data["title"];
+    const movieDesc = data["overview"];
+    const movieRate = Math.round(data["vote_average"] * 10) / 10;
+    const movieImg = data["poster_path"];
 
     // 영화 데이터를 HTML에 표시
     let temp_html = `<div class="movie-box">
@@ -62,18 +62,18 @@ fetch(movieUrl, options)
   });
 
 function open_box() {
-  const reviewBox = document.getElementById('reviewBox');
-  const reviewBoxDisplay = localStorage.getItem('reviewBoxDisplay');
+  const reviewBox = document.getElementById("reviewBox");
+  const reviewBoxDisplay = localStorage.getItem("reviewBoxDisplay");
 
   switch (reviewBoxDisplay) {
-    case 'visible':
-      reviewBox.style.display = 'none';
-      localStorage.setItem('reviewBoxDisplay', 'hidden');
+    case "visible":
+      reviewBox.style.display = "none";
+      localStorage.setItem("reviewBoxDisplay", "hidden");
       break;
-    case 'hidden':
+    case "hidden":
     default:
-      reviewBox.style.display = 'block';
-      localStorage.setItem('reviewBoxDisplay', 'visible');
+      reviewBox.style.display = "block";
+      localStorage.setItem("reviewBoxDisplay", "visible");
       break;
   }
 }
@@ -81,10 +81,25 @@ function open_box() {
 // 평점 색 구분
 function getRatingColor(rate) {
   if (rate >= 8) {
-    return 'green';
+    return "green";
   } else if (rate >= 5) {
-    return 'orange';
+    return "orange";
   } else {
-    return 'red';
+    return "red";
   }
 }
+
+clickButton.addEventListener("click", renderMainpage);
+searchBox.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    renderMainpage();
+  }
+});
+//메인 페이지랑 똑같습니다.
+
+function renderMainpage() {
+  const movieTitle = searchBox.value;
+  return (window.location.href = `/main_page/main_pro8.html?title=${movieTitle}`);
+}
+//url 전달함수입니다.
