82a61c121b014716ad6cf4c2c8a37c7e4cbe4fad
Kim Jinkyung <jinkyung127@naver.com>
2023-06-05 12:01:46 +0900

    review sample

--- sub_page/sub_pro8.js
+++ sub_page/sub_pro8.js
@@ -1,30 +1,30 @@
 // URL에서 id 추출
 const urlParams = new URLSearchParams(window.location.search);
-const movieId = urlParams.get('id');
+const movieId = urlParams.get("id");
 
 const options = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
+    accept: "application/json",
     Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOWJiOTJmNjQ4ZDQxOTExNTVkMTdjOGU0M2YyNWU2OCIsInN1YiI6IjY0NzIxZmM1ZGQ3MzFiMDBjMGJhYWU5MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Y3E-hG7rguBwXeMYwJDAuXRxZp8nBSidYbJb6AFhSf0',
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOWJiOTJmNjQ4ZDQxOTExNTVkMTdjOGU0M2YyNWU2OCIsInN1YiI6IjY0NzIxZmM1ZGQ3MzFiMDBjMGJhYWU5MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Y3E-hG7rguBwXeMYwJDAuXRxZp8nBSidYbJb6AFhSf0",
   },
 };
 
-const apiKey = 'e9bb92f648d4191155d17c8e43f25e68&language=ko';
+const apiKey = "e9bb92f648d4191155d17c8e43f25e68&language=ko";
 
-const movieDetailsContainer = document.getElementById('movie-details');
+const movieDetailsContainer = document.getElementById("movie-details");
 
 const movieUrl = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=ko`;
 
 fetch(movieUrl, options)
   .then((response) => response.json())
   .then((data) => {
-    const movieTitle = data['title'];
-    const movieDesc = data['overview'];
-    const movieRate = Math.round(data['vote_average'] * 10) / 10;
+    const movieTitle = data["title"];
+    const movieDesc = data["overview"];
+    const movieRate = Math.round(data["vote_average"] * 10) / 10;
 
-    const movieImg = data['poster_path'];
+    const movieImg = data["poster_path"];
 
     // 영화 데이터를 HTML에 표시
     let temp_html = `<div class="movie-box">
@@ -39,7 +39,6 @@ fetch(movieUrl, options)
                                       movieRate
                                     )}">&nbsp;★${movieRate}</p></div>
                                 </div>
-                               
                                 <button onclick="open_box()" class="comment-postbtn">Comments</button>
                                 <div class="reviewBox" id="reviewBox" style="display: none;">
                                   <div class="userIpt">
@@ -57,23 +56,41 @@ fetch(movieUrl, options)
                                 </div>
                             </div>
                         </div>
-                    </div>`;
+                    </div>
+                    <div class="review-list-box">
+                      <div class="mycards" id="review-list">
+                        <p></p>
+                        <div class="review-card">
+                            <div class="review-card-body">
+                              <header class="name-header">익명</header>
+                              <p>리뷰 남기기 샘플</p>
+                            </div>
+                        </div>
+                        <div class="review-card">
+                            <div class="review-card-body">
+                              <header class="name-header">익명</header>
+                              <p>리뷰 남기기 샘플</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                    `;
     movieDetailsContainer.innerHTML = temp_html;
   });
 
 function open_box() {
-  const reviewBox = document.getElementById('reviewBox');
-  const reviewBoxDisplay = localStorage.getItem('reviewBoxDisplay');
+  const reviewBox = document.getElementById("reviewBox");
+  const reviewBoxDisplay = localStorage.getItem("reviewBoxDisplay");
 
   switch (reviewBoxDisplay) {
-    case 'visible':
-      reviewBox.style.display = 'none';
-      localStorage.setItem('reviewBoxDisplay', 'hidden');
+    case "visible":
+      reviewBox.style.display = "none";
+      localStorage.setItem("reviewBoxDisplay", "hidden");
       break;
-    case 'hidden':
+    case "hidden":
     default:
-      reviewBox.style.display = 'block';
-      localStorage.setItem('reviewBoxDisplay', 'visible');
+      reviewBox.style.display = "block";
+      localStorage.setItem("reviewBoxDisplay", "visible");
       break;
   }
 }
@@ -81,14 +98,14 @@ function open_box() {
 // 평점 색 구분
 function getRatingColor(rate) {
   if (rate >= 8) {
-    return 'green';
+    return "green";
   } else if (rate >= 5) {
-    return 'orange';
+    return "orange";
   } else {
-    return 'red';
+    return "red";
   }
 }
 
 const main = () => {
-  window.location.href = '/main_page/main_pro8.html';
+  window.location.href = "/main_page/main_pro8.html";
 };
