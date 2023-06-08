3f218d1446f8aa86e1fc82c4b106db0ca171e3ab
marshmello <pesbg123@gmail.com>
2023-06-08 21:11:27 +0900

    kakao_api add

--- main_page/main_pro8.js
+++ main_page/main_pro8.js
@@ -1,55 +1,61 @@
 const options = {
-  method: "GET",
+  method: 'GET',
   headers: {
-    accept: "application/json",
+    accept: 'application/json',
     Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOWJiOTJmNjQ4ZDQxOTExNTVkMTdjOGU0M2YyNWU2OCIsInN1YiI6IjY0NzIxZmM1ZGQ3MzFiMDBjMGJhYWU5MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Y3E-hG7rguBwXeMYwJDAuXRxZp8nBSidYbJb6AFhSf0",
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOWJiOTJmNjQ4ZDQxOTExNTVkMTdjOGU0M2YyNWU2OCIsInN1YiI6IjY0NzIxZmM1ZGQ3MzFiMDBjMGJhYWU5MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Y3E-hG7rguBwXeMYwJDAuXRxZp8nBSidYbJb6AFhSf0',
   },
 };
 // SH 상세페이지 검색 url
 const urlParamsSH = new URLSearchParams(window.location.search);
-const sub_movieTitle = urlParamsSH.get("title");
+const sub_movieTitle = urlParamsSH.get('title');
 
-const apiKey = "e9bb92f648d4191155d17c8e43f25e68&language=ko";
+const apiKey = 'e9bb92f648d4191155d17c8e43f25e68&language=ko';
 
-const movieCardBox = document.getElementById("cards-box");
-const popularTab = document.getElementById("popular-category");
-const nowPlayingTab = document.getElementById("nowplaying-category");
-const topRatedTab = document.getElementById("toprate-category");
-const upcomingTab = document.getElementById("upcoming-category");
-const clickButton = document.getElementById("click-btn");
-const searchBox = document.getElementById("search-box");
-const mainH1 = document.getElementById("mainH1");
+const movieCardBox = document.getElementById('cards-box');
+const popularTab = document.getElementById('popular-category');
+const nowPlayingTab = document.getElementById('nowplaying-category');
+const topRatedTab = document.getElementById('toprate-category');
+const upcomingTab = document.getElementById('upcoming-category');
+const clickButton = document.getElementById('click-btn');
+const searchBox = document.getElementById('search-box');
+const mainH1 = document.getElementById('mainH1');
+const kakaobtn = document.getElementById('click-kakao');
 
-const popular = "https://api.themoviedb.org/3/movie/popular?";
-const NowPlaying = "https://api.themoviedb.org/3/movie/now_playing?";
-const TopRated = "https://api.themoviedb.org/3/movie/top_rated?";
-const Upcoming = "https://api.themoviedb.org/3/movie/upcoming?";
+const popular = 'https://api.themoviedb.org/3/movie/popular?';
+const NowPlaying = 'https://api.themoviedb.org/3/movie/now_playing?';
+const TopRated = 'https://api.themoviedb.org/3/movie/top_rated?';
+const Upcoming = 'https://api.themoviedb.org/3/movie/upcoming?';
 
 // 상세페이지에서 url로 넘겨준 domain = Popular를 가져옴
 const urlParamsJh = new URLSearchParams(window.location.search);
-const CD = urlParamsJh.get("domain");
+const CD = urlParamsJh.get('domain');
 
 const url = (movieUrl) =>
-  showMovieList(movieUrl + "api_key=" + apiKey, options);
+  showMovieList(movieUrl + 'api_key=' + apiKey, options);
 
 const cardsRemove = (movieUrl) => {
   // movieCardBox.textContent = '';
   url(movieUrl);
 };
 
+// 카카오 검색 api 버튼 이벤트 지정
+kakaobtn.addEventListener('click', () => {
+  window.open('http://localhost:5002/main_page/kakao.html');
+});
+
 const showMovieList = (moviesUrl) => {
   fetch(moviesUrl, options)
     .then((response) => response.json())
     .then((data) => {
-      let rows = data["results"];
-      movieCardBox.textContent = "";
+      let rows = data['results'];
+      movieCardBox.textContent = '';
       rows.forEach((item) => {
-        let movieTitle = item["title"];
-        let movieDesc = item["overview"];
-        let movieRate = Math.round(item["vote_average"] * 10) / 10;
-        let movieImg = item["poster_path"];
-        let movieId = item["id"];
+        let movieTitle = item['title'];
+        let movieDesc = item['overview'];
+        let movieRate = Math.round(item['vote_average'] * 10) / 10;
+        let movieImg = item['poster_path'];
+        let movieId = item['id'];
 
         let temp_html = `<div class="col" style="color: white">
                              <div class="solo-card" style="background-color: rgb(58, 58, 57)">
@@ -65,13 +71,13 @@ const showMovieList = (moviesUrl) => {
                             </div>
                         </div>`;
 
-        movieCardBox.insertAdjacentHTML("beforeend", temp_html);
+        movieCardBox.insertAdjacentHTML('beforeend', temp_html);
 
         const clickCardBox = document.getElementById(`cardPost-${movieId}`);
-        clickCardBox.addEventListener("click", () => clickCard(movieId));
+        clickCardBox.addEventListener('click', () => clickCard(movieId));
 
         const clickDescBox = document.getElementById(`desc-body-${movieId}`);
-        clickDescBox.addEventListener("click", () => clickDesc(movieId));
+        clickDescBox.addEventListener('click', () => clickDesc(movieId));
       });
     });
 };
@@ -90,11 +96,11 @@ const searchMovie = (searchBoxValue) => {
   fetch(searchUrl, options)
     .then((response) => response.json())
     .then((data) => {
-      const results = data["results"];
+      const results = data['results'];
       if (results.length === 0) {
-        alert("일치하는 검색 결과를 찾을 수 없습니다.");
+        alert('일치하는 검색 결과를 찾을 수 없습니다.');
       } else {
-        movieCardBox.textContent = "";
+        movieCardBox.textContent = '';
 
         results.forEach((item) => {
           const movieTitle = item.title;
@@ -117,23 +123,23 @@ const searchMovie = (searchBoxValue) => {
                                 </div>
                             </div>`;
 
-          movieCardBox.insertAdjacentHTML("beforeend", temp_html);
+          movieCardBox.insertAdjacentHTML('beforeend', temp_html);
           const clickCardBox = document.getElementById(`cardPost-${movieId}`);
-          clickCardBox.addEventListener("click", () => clickCard(movieId));
+          clickCardBox.addEventListener('click', () => clickCard(movieId));
           // 카드 설명부분 클릭시 상세페이지로 넘어가는 이벤트
           const clickDescBox = document.getElementById(`desc-body-${movieId}`);
-          clickDescBox.addEventListener("click", () => clickDesc(movieId));
+          clickDescBox.addEventListener('click', () => clickDesc(movieId));
         });
       }
     });
 };
 
-clickButton.addEventListener("click", () => {
+clickButton.addEventListener('click', () => {
   const searchBoxValue = searchBox.value;
   searchMovie(searchBoxValue);
 });
-searchBox.addEventListener("keypress", (event) => {
-  if (event.key === "Enter") {
+searchBox.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
     event.preventDefault();
     const searchBoxValue = searchBox.value;
     searchMovie(searchBoxValue);
@@ -142,17 +148,17 @@ searchBox.addEventListener("keypress", (event) => {
 // searchMovie함수가 변수를 받는 형식으로 바꿨습니다. 메인페이지와 서브페이지 둘다
 // 검색 기능을 쓰기때문에 변수차이로 로직이 구성됩니다.
 const main = () => {
-  window.location.href = "/main_page/main_pro8.html";
+  window.location.href = '/main_page/main_pro8.html';
 };
-mainH1.addEventListener("click", main);
+mainH1.addEventListener('click', main);
 
 const getRatingColor = (rate) => {
   if (rate >= 8) {
-    return "green";
+    return 'green';
   } else if (rate >= 5) {
-    return "orange";
+    return 'orange';
   } else {
-    return "red";
+    return 'red';
   }
 };
 
@@ -160,25 +166,25 @@ const getRatingColor = (rate) => {
 url(TopRated);
 
 // HK 각각의 탭이 클릭되었을 때 해당 카테고리에 대한 cardsRemove 함수를 호출하는 이벤트 리스너를 등록
-popularTab.addEventListener("click", () => cardsRemove(popular));
-nowPlayingTab.addEventListener("click", () => cardsRemove(NowPlaying));
-topRatedTab.addEventListener("click", () => cardsRemove(TopRated));
-upcomingTab.addEventListener("click", () => cardsRemove(Upcoming));
+popularTab.addEventListener('click', () => cardsRemove(popular));
+nowPlayingTab.addEventListener('click', () => cardsRemove(NowPlaying));
+topRatedTab.addEventListener('click', () => cardsRemove(TopRated));
+upcomingTab.addEventListener('click', () => cardsRemove(Upcoming));
 
 // HK CD 변수의 값을 기반으로 각 카테고리에 대한 cardsRemove 함수를 호출
-if (CD === "Popular") {
+if (CD === 'Popular') {
   cardsRemove(popular);
-} else if (CD === "NowPlaying") {
+} else if (CD === 'NowPlaying') {
   cardsRemove(NowPlaying);
-} else if (CD === "TopRated") {
+} else if (CD === 'TopRated') {
   cardsRemove(TopRated);
-} else if (CD === "Upcoming") {
+} else if (CD === 'Upcoming') {
   cardsRemove(Upcoming);
 }
 
 // sub_movieTitle이 서브페이지에서 검색했을 때 변수로 오는 값입니다.
 // 값이 비어있지 않다면 메인페이지에 서브페이지에서 검색한 값을 보여줍니다.
 if (sub_movieTitle !== null) {
-  movieCardBox.textContent = "";
+  movieCardBox.textContent = '';
   searchMovie(sub_movieTitle);
 }
