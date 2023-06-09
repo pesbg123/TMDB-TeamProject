94b3dc77242040883de61a5daa835b060501d50d
jrmun <jrmun123@gmail.com>
2023-06-09 11:11:11 +0900

    main함수 export.js로 옮김

--- modules/export.js
+++ modules/export.js
@@ -5,11 +5,11 @@ export const openGitHubProfile = (url) => {
 
 // 배열안에 array로 각각 깃헙 주소 할당
 export const teamMembers = [
-  { id: 'JH', url: 'https://github.com/pesbg123' },
-  { id: 'SH', url: 'https://github.com/jrmun' },
-  { id: 'JK', url: 'https://github.com/jinkyung127' },
-  { id: 'HW', url: 'https://github.com/hyunwoo87' },
-  { id: 'HK', url: 'https://github.com/kwakhyunkyu' },
+  { id: "JH", url: "https://github.com/pesbg123" },
+  { id: "SH", url: "https://github.com/jrmun" },
+  { id: "JK", url: "https://github.com/jinkyung127" },
+  { id: "HW", url: "https://github.com/hyunwoo87" },
+  { id: "HK", url: "https://github.com/kwakhyunkyu" },
 ];
 
 // 상세페이지에서 카테고리들로 넘어가는 함수
@@ -17,8 +17,13 @@ export const goToDomain = (domain) =>
   (window.location.href = `/main_page/main_pro8.html?domain=${domain}`);
 
 export const categorys = [
-  { id: 'popular-category', category: 'Popular' },
-  { id: 'nowplaying-category', category: 'NowPlaying' },
-  { id: 'toprate-category', category: 'TopRated' },
-  { id: 'upcoming-category', category: 'Upcoming' },
+  { id: "popular-category", category: "Popular" },
+  { id: "nowplaying-category", category: "NowPlaying" },
+  { id: "toprate-category", category: "TopRated" },
+  { id: "upcoming-category", category: "Upcoming" },
 ];
+
+// JH hearder Carrot Movie 클릭시 main페이지로 넘어가는 함수
+export const main = () => {
+  window.location.href = "/main_page/main_pro8.html";
+};
