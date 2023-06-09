2c5cde1274eaef6f3e37befa45283edb0f494f69
marshmello <pesbg123@gmail.com>
2023-06-09 13:08:55 +0900

    import /TMDB-TeamProject경로 추가

--- modules/import.js
+++ modules/import.js
@@ -4,16 +4,16 @@ import {
   teamMembers,
   goToDomain,
   categorys,
-} from "/modules/export.js";
+} from '/TMDB-TeamProject/modules/export.js';
 
 // footer에 있는 팀원별 버튼 클릭 이벤트 지정
 teamMembers.forEach((member) => {
   const clickMember = document.getElementById(member.id);
-  clickMember.addEventListener("click", () => openGitHubProfile(member.url));
+  clickMember.addEventListener('click', () => openGitHubProfile(member.url));
 });
 
 // 서브페이지 카테고리 하나당 클릭 이벤트 지정
 categorys.forEach((categorys) => {
   const clickCategory = document.getElementById(categorys.id);
-  clickCategory.addEventListener("click", () => goToDomain(categorys.category));
+  clickCategory.addEventListener('click', () => goToDomain(categorys.category));
 });
